24e1a0d7d5edd87b28795d9757b01b364c902230
olga_ogarkova <olga_ogarkova@epam.com>
2023-01-11 18:49:09 +0300

    Start proj

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
+import pygame
 
-if __name__ == '__main__':
-    print_hi('PyCharm')
+height, width = 800, 600
+win = pygame.display.set_mode((height, width))
+pygame.display.set_caption("brick breaker")
 
+def main():
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+    pygame.quit
+    quit()
+
+if __name__ == "_main_":
+    main()
+# if __name__ == '__main__':
+#     print_hi('PyCharm')
